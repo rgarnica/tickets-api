65fe1d18d812cd42cce376f4edab19f63a6b83d9
Raphael Garnica <rgarnica@localhost.localdomain>
2018-09-23 22:08:42 -0300

    Command for ticket classification

--- app/Console/Commands/ClassifyTicketsCommand.php
+++ app/Console/Commands/ClassifyTicketsCommand.php
@@ -2,21 +2,63 @@
 
 namespace App\Console\Commands;
 
+use Phpml\Dataset\CsvDataset;
 use Illuminate\Console\Command;
-use App\Repositories\TicketsRepositoryInterface as TicketsRepository;
+use App\Services\SentimentAnalyser;
+use App\Services\TicketDateAnalyser;
+use Phpml\Classification\NaiveBayes;
+use Phpml\Tokenization\WordTokenizer;
+use App\Services\TicketSentimentAnalyser;
+use Phpml\FeatureExtraction\TfIdfTransformer;
+use Phpml\FeatureExtraction\TokenCountVectorizer;
+use App\Repositories\TicketRepositoryInterface as TicketRepository;
 
 class ClassifyTicketsCommand extends Command
 {
 
-    protected $signature = "tickets:classify";
+    protected $signature = 'tickets:classify
+                            {dataset_path : Path to training dataset}
+                           ';
 
-    protected $description = "Classify tickets priority between Neutral and High";
+    protected $description = "Classify tickets priority between Neutral and High priority";
 
 
-    public function handle(TicketsRepository $ticketsRepo)
+    public function handle(TicketRepository $ticketsRepo)
     {
-        $this->info("Getting all tickets");
+        $pathToDataset = $this->argument('dataset_path');
+
+        $this->line("Getting all tickets");
         $tickets = $ticketsRepo->get();
+        $this->line("Creating classification parameters");
+        $dataset = new CsvDataset(storage_path($pathToDataset), 1);
+        $vectorizer = new TokenCountVectorizer(new WordTokenizer());
+        $tfIdfTransformer = new TfIdfTransformer();
+        $classifier = new NaiveBayes();
+        $analyser = new SentimentAnalyser($vectorizer, $tfIdfTransformer, $classifier);
+
+
+        $this->line("Training the classificator");
+        $analyser->train($dataset);
+
+        $this->line("Classifiyng tickets...");
+
+        $tickets = $tickets->map(function($ticket) use ($analyser) {
+            $an = [];
+            $an[] = new TicketDateAnalyser($ticket, 0.20);
+            $an[] = new TicketSentimentAnalyser($analyser, $ticket, 0.80);
+            $points = 0;
+            foreach ($an as $classification) {
+                $points += $classification->run();
+            }
+            
+            $ticket["PriorityPoints"] = $points;
+            $ticket["PriorityLabel"] = ($points > 0.50) ? "Alta" : "Normal"; 
+            return $ticket;
+        });
+
+        $ticketsRepo->save($tickets);
+        $this->info("Tickets classified successfully!");
+        
     }
 
 }
